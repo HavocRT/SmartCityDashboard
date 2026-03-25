729ec10d9cb18ecbaff603fb52a3503e8cbb8916
HavocRT <anushjohn2005@gmail.com>
2026-03-25 20:42:09 +0530

    added culture route file

---
+++ backend/routes/culture.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+const dataPath = (file) => path.join(__dirname, "../data/culture", file);
+
+router.get("/upcoming-events", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath("upcoming-events.json"), "utf-8"));
+  res.json(data);
+});
+
+router.get("/museums", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath("museums.json"), "utf-8"));
+  res.json(data);
+});
+
+router.get("/public-libraries", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath("public-libraries.json"), "utf-8"));
+  res.json(data);
+});
+
+router.get("/tourism-analytics", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath("tourism-analytics.json"), "utf-8"));
+  res.json(data);
+});
+
+router.get("/public-wifi-zones", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath("public-wifi-zones.json"), "utf-8"));
+  res.json(data);
+});
+
+module.exports = router;
